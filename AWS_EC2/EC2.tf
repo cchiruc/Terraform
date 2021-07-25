408bef9a5d905e12690acf4ac8091ccb3c564404
chiru <chiranjeevi.edlapalle@gmail.com>
2021-07-26 02:15:31 +0800

    V14

--- AWS_EC2/EC2.tf
+++ AWS_EC2/EC2.tf
@@ -2,6 +2,7 @@
 resource "aws_instance" "chiru_server" {
   ami           = "ami-074df373d6bafa625"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.chiru_allow_all.id]
 
   tags = {
     Name = "chiru_server"
@@ -11,4 +12,30 @@ resource "aws_instance" "chiru_server" {
 ## Defining the region
 provider "aws" {
   region  = "us-east-1"
+}
+
+resource "aws_security_group" "chiru_allow_all" {
+  name        = "chiru_allow_all"
+  description = "Allow all inbound traffic"
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 0-65556
+protocol         = "tcp"
+cidr_blocks      = ["0.0.0.0/0"]
+
+}
+
+egress {
+from_port        = 0
+to_port          = 0
+protocol         = "-1"
+cidr_blocks      = ["0.0.0.0/0"]
+
+}
+
+tags = {
+Name = "chiru_allow_all"
+}
 }
