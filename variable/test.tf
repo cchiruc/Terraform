f8edd3db414a395c37751ce6ddcdfde0cb1bf9fc
chiru <chiranjeevi.edlapalle@gmail.com>
2021-07-26 21:49:47 +0800

    V2

--- variable/test.tf
+++ variable/test.tf
@@ -3,3 +3,28 @@ resource "aws_instance" "Frontend_server"  {
     instance_type = "t2.micro"
 }
 
+provider "aws" {
+  region  = "us-east-1"
+}
+
+resource "aws_security_group" "allow_all" {
+  name = "test_nsg"
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 0
+    to_port          = 10000
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [
+      "0.0.0.0/0"]
+
+  }
+}
+
