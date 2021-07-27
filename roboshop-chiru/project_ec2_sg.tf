1a7bb2f29561671798744ebf045840a1de119584
chiru <chiranjeevi.edlapalle@gmail.com>
2021-07-28 01:37:18 +0800

    roboshop chiru v1.5.8

--- roboshop-chiru/project_ec2_sg.tf
+++ roboshop-chiru/project_ec2_sg.tf
@@ -39,7 +39,7 @@ resource "aws_ec2_tag" "roboshop_instance" {
 }
 # pull the shall scripting code for installtion and configration of roboshop applicaitons.
 resource "aws_route53_record" "roboshop_internal" {
-  count = length(var.COMPONENTS)
+  count   = length(var.COMPONENTS)
   zone_id = "Z04635172URQKFFOUCBQ5"
   name    = element(var.COMPONENTS,count.index )
   type    = "A"
