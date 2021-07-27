ac3c9b355ddf6f0e798168548b21f6b8e7fedd69
chiru <chiranjeevi.edlapalle@gmail.com>
2021-07-28 00:57:15 +0800

    test.dns

--- roboshop-chiru/project_ec2_sg.tf
+++ roboshop-chiru/project_ec2_sg.tf
@@ -37,6 +37,18 @@ resource "aws_ec2_tag" "roboshop_instance" {
   key         = "Name"
   value       = element(var.COMPONENTS, count.index )
 }
+# pull the shall scripting code for installtion and configration of roboshop applicaiton.
+resource "aws_route53_record" "roboshop.internal" {
+  count = length(var.COMPONENTS)
+  zone_id = Z04635172URQKFFOUCBQ5
+  name    = element(var.COMPONENTS,count.index )
+  type    = "A"
+  ttl     = "300"
+  records = [element(aws_spot_instance_request.project_roboshop.*.private_ip, count.index )]
+}
+
+
+
 
 
 
